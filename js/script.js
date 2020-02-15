c083a015c94d3c07a89b134638017a9207a65acb
cclzhang <hello@cecilezhang.com>
2020-02-15 16:05:01 -0500

    added more js behind the scenes functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
+const eQuiz = {};
+
 // doc ready
 $(()=>{
     eQuiz.init();
 });
-
-const eQuiz = {};
 
 // eQuiz initialization
 eQuiz.init = () => {
@@ -11,9 +11,11 @@ eQuiz.init = () => {
     eQuiz.getDom();
     eQuiz.setGlobal();
     eQuiz.listenUp();
+
 };
 
-
+// E = elephant
+// use ajax to get the API
 eQuiz.getE = () => {
     $.ajax({
         // const elephantUrl = `https://elephant-api.herokuapp.com/`;
@@ -27,27 +29,26 @@ eQuiz.getE = () => {
             xmlToJSON: false,
             useCache: false
         }
-    }).then(function (res) {
-        eQuiz.gatherE(res);
-        eQuiz.gatherFakeAns(res);
+    }).then(function (data) {
+        eQuiz.gatherE(data);
+        eQuiz.gatherFakeAns(data);
     });
 };
 
 eQuiz.gatherE = function(data) {
-    const arrayOfE = [];
+    eQuiz.arrayOfE = [];
     data.forEach(function(elephant) {
         //process complete data only into array
         if (elephant.image !== "https://elephant-api.herokuapp.com/pictures/missing.jpg" && elephant.name !== undefined && elephant.species !== "Unavailable" && elephant.sex !== "Unavailable") {
-            arrayOfE.push({name: elephant.name, affiliation: elephant.affiliation, species: elephant.species, sex: elephant.sex, image: elephant.image, note: elephant.note});
+            eQuiz.arrayOfE.push({name: elephant.name, affiliation: elephant.affiliation, species: elephant.species, sex: elephant.sex, image: elephant.image, note: elephant.note});
         }
     });
     //randomly select 5 elephants for 5 questions
-    eQuiz.fiveOfE = [];
-    for (let i = 0; i < 5; i++) {
-        eQuiz.fiveOfE.push(eQuiz.randomize(arrayOfE));
-        //future error handling to address: this could result in the same elephant for multiple questions which is okay as long as the questions are not the same. we need to either: 1. implement a check for assigning diff questions if there's a repeated elephant in this array OR 2. remove this possibility of the same elephant repeated in the first place
-    }
+    eQuiz.shuffledE = [];
+    eQuiz.shuffle(eQuiz.shuffledE, eQuiz.arrayOfE);
 }
+
+
 
 eQuiz.gatherFakeAns = function(data) {
     //gather names
@@ -57,6 +58,7 @@ eQuiz.gatherFakeAns = function(data) {
             eQuiz.fakeNames = [];
             eQuiz.fakeNames.push(elephant.name);
         }
+        // add an if statement if fake answer = real answer, do not push
     });
     //gather locations
     data.forEach(function(elephant) {
@@ -75,7 +77,7 @@ eQuiz.getDom = () => {
     eQuiz.$main = $("main");
     // 
     eQuiz.$qScreen = $(".questionScreen");
-    eQuiz.$question = $(".question");
+    eQuiz.$questionForm = $(".question");
     eQuiz.$hintToaster = $(".hintToaster");
     // 
     eQuiz.$infoCard = $(".infoCard");
@@ -85,15 +87,16 @@ eQuiz.getDom = () => {
 
 //set global variables
 eQuiz.setGlobal = function() {
-    //variable to work through the fiveOfE array properly when assigning answers
+    //variable to work through the shuffledE array properly when assigning answers
     eQuiz.qNum = 0;
     //variable to assign answer set for the current question
-    eQuiz.ansArray = [];
+    eQuiz.ansArray = ["baby", "toddler"];
+    eQuiz.randIndex = 0;
 }
 
 //set up event listeners
 eQuiz.listenUp = function() {
-    eQuiz.$startB.one('click', eQuiz.startQuiz);
+    eQuiz.$startB.on('click', eQuiz.startQuiz);
 }
 
 //start the quiz
@@ -107,15 +110,30 @@ eQuiz.startQuiz = function() {
     eQuiz.setQ();
     eQuiz.setA();
     //load question
-    const htmlToAdd = `
+    // const radioHtml = "";
     
+
+    eQuiz.ansArray.forEach(function (item) {
+        eQuiz.$questionForm.html(`
+            <input type="radio" name="" id="">
+            <label for="">${item}</label>
+        `);
+    });
+    console.log("hi");
+    const answerHtml = `
+
     `;
-    eQuiz.$question.html(htmlToAdd);
+    const htmlToAdd = `
+
+    `;
+    eQuiz.$questionForm.html(htmlToAdd);
     //load hint
 }
 
 eQuiz.nextQ = function() {
     eQuiz.qNum++;
+    eQuiz.setQ();
+    eQuiz.setA();
     //if statement to check if quiz is over
 }
 
@@ -124,6 +142,8 @@ eQuiz.qBank = ["What is my sex?", "What species am I?", "What is my name?", "Whe
 eQuiz.setQ = function() {
     eQuiz.currentQ = eQuiz.randomize(eQuiz.qBank);
 };
+
+
 
 eQuiz.setA = function() {
     if (eQuiz.currentQ === "What is my sex?") {
@@ -138,27 +158,49 @@ eQuiz.setA = function() {
 }
 
 eQuiz.getSex = function() {
+    // set 2, validate chosen answer
+    console.log("sex")
+}
+
+eQuiz.getSpecies = function() {
+    // set 3, validate chosen answer
+    console.log("getSpecies")
+}
+
+eQuiz.getNames = function() {
     //grab correct elephant name
-    eQuiz.ansArray.push(eQuiz.fiveOfE[eQuiz.qNum].name);
-    //grab 3 incorrect elephant names
+    eQuiz.ansArray.push(eQuiz.shuffledE[eQuiz.qNum].name);
+
+    // grab 3 incorrect elephant names
     for (let i = 0; i < 4; i++) {
         eQuiz.ansArray.push(eQuiz.randomize(eQuiz.fakeNames));
         //future error handling: we need to make sure no doubles with an includes check maybe?
     }
+
+
+
     console.log(eQuiz.ansArray);
-}
-
-eQuiz.getSpecies = function() {
-
-}
-
-eQuiz.getNames = function() {
-
+    
 }
 
 eQuiz.getLocations = function() {
-
+    console.log("location");
 }
+
+
+
+eQuiz.shuffle = function (tempArray, originalArray) {
+    while (tempArray.length < originalArray.length) {
+        this.randIndex = Math.floor(Math.random() * originalArray.length);
+        if (tempArray.includes(originalArray[this.randIndex]) === false) {
+            tempArray.push(originalArray[this.randIndex]);
+        }
+    }
+}
+
+// original array = all elephants from API
+// new array = 5 chosen random elephants that don't repeat
+// generate a random number
 
 eQuiz.randomize = (array) => {
     return array[Math.floor(Math.random() * array.length)];
